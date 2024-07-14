8fb44ba486344982b7e9f70a246543165f7bf4aa
brimutuma <49781893+brimutuma@users.noreply.github.com>
2024-07-14 18:32:42 +0300

    Organizational
    Moved all files to src folder
    Added signup gui file

---
+++ src/UserSignupGUI.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class UserSignupGUI {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        frame.setSize(100, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(panel);
+
+    }
+}
